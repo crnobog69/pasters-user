705a726ab4a9421529ac949b555ed150771106eb
crnobog <carskalavra@proton.me>
2024-10-24 14:45:52 +0200

    ❄️

---
+++ push.ps1
@@ -0,0 +1,11 @@
+# Скрипта која аутоматизује `git add`, `git commit` и `git push` за Windows
+# Покретање: отворите PowerShell и покрените ову скрипту
+
+# Додај све промене
+git add .
+
+# Направи commit са стандардном поруком
+git commit -m "❄️"
+
+# Пошаљи промене на GitHub (или други remote)
+git push github main
---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Скрипта која аутоматизује `git add`, `git commit` и `git push` за Windows
+# Покретање: отворите PowerShell и покрените ову скрипту
+
+# Додај све промене
+git add .
+
+# Направи commit са стандардном поруком
+git commit -m "❄️"
+
+# Пошаљи промене на GitHub (или други remote)
+git push github main
